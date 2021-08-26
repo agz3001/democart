b6562f7bdccb70cc06b880c61881017010ea7751
Fuji <agz3001@yahoo.co.jp>
2021-08-26 16:07:43 +0900

    rechange

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -135,7 +135,7 @@
       </footer>
     </div>
     <!-- js-cookie -->
-    <script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
     <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
     <!--<script src="{{ mix('js/style.js') }}" type="module"></script>-->
 
